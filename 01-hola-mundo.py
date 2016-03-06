a9cc86bd4ca21fa5eed86114e593dd19efd7b549
raichuk <seba.alvarez16@gmail.com>
2016-03-05 21:47:51 -0300

    Se sube 01-hola-mundo

---
+++ 01-hola-mundo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import chispaengine
+
+chispa = chispaengine.prender()
+
+camara = chispa.camaras.Netbook()
+camara.prender()
+
+while(True):
+    imagen = camara.traer_imagen()
+    imagen.mostrar("Ventana")
+
+    chispa.esperar(0.025)
